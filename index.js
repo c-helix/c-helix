9b57f3ad197c983b97077c7df497022d7b0f100a
Chetan Sharma <iamchetansharma8>
2021-04-04 18:31:03 +0530

    preparing for deploy

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is listening at http://localhost:3000`);
+});
